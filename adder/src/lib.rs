3bb8353c1b30c5e4ff09598df5fe6f76bb9cc560
Psyhackological <konradkon@duck.com>
2022-11-23 12:09:51 +0100

    Updated to Rust book code

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn exploration() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(2 + 2, 4);
     }
 }
