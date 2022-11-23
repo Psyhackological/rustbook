3aa7611f45acfb6a0d11bc97ca3d3b99271aa3c5
Psyhackological <konradkon@duck.com>
2022-11-23 12:32:15 +0100

    Testing add_two function

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,12 +1,12 @@
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn exploration() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
-    fn another() {
-        panic!("Make this test fail");
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
     }
 }
