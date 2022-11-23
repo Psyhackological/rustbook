56dc5248e5c67ea47ba42a141b20072547a1445b
Psyhackological <konradkon@duck.com>
2022-11-23 23:17:14 +0100

    More testing

--- guessing_game/src/lib.rs
+++ guessing_game/src/lib.rs
@@ -5,7 +5,9 @@ pub struct Guess {
 impl Guess {
     pub fn new(value: i32) -> Self {
         if value < 1 {
-            panic!("Guess value must be between 1 and 100, got {}.", value);
+            panic!("Guess value must be greater than or equal to 1, got {}.", value);
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {}.", value);
         }
 
         Self { value }
@@ -17,7 +19,7 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "less than or equal to 100")]
     fn greater_than_100() {
         Guess::new(200);
     }
