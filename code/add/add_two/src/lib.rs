b7a2cac22d719f2f3fbdcc1219a4370187f209b3
Psyhackological <konradkon@duck.com>
2022-12-31 14:43:58 +0100

    Finished Chapter 14.3

--- code/add/add_two/src/lib.rs
+++ code/add/add_two/src/lib.rs
@@ -1,7 +1,7 @@
 use rand;
 
-pub fn add_one(x: i32) -> i32 {
-    x + 1
+pub fn add_two(x: i32) -> i32 {
+    x + 2
 }
 
 #[cfg(test)]
@@ -10,6 +10,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(3, add_one(2));
+        assert_eq!(3, add_two(1));
     }
 }
