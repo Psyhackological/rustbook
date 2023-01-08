e15b2324e9bf03cb72c864b6e2ab2ac4b2a20166
Psyhackological <konradkon@duck.com>
2023-01-08 16:14:59 +0100

    Cargo clippy fix

--- code/exercises/temp_converter/src/main.rs
+++ code/exercises/temp_converter/src/main.rs
@@ -2,8 +2,8 @@ use temp_converter::{Celsius, Kelvin};
 
 fn main() {
     let kelvin = Kelvin::new(273.15);
-    println!("{:?}", kelvin);
+    println!("{kelvin:?}");
 
     let celsius = Celsius::from(kelvin);
-    println!("{:?}", celsius);
+    println!("{celsius:?}");
 }
