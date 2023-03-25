311cb67e112fcfbfc3309b20003ed03b442d5436
Psyhackological <konradkon@duck.com>
2023-03-25 12:28:24 +0100

    Replaced mv with git mv

--- md_notes/sh/rename_md.sh
+++ md_notes/sh/rename_md.sh
@@ -4,7 +4,7 @@ function rename_markdown_files() {
 	for old_md in *.md; do
 		new_md=$(echo "$old_md" | sed -E 's/^([0-9]+)\.([0-9]+)\.\s*(.*)\.md/\1\.\2_\L\3\.md/' | sed 's/ /_/g' | sed 's/__\?/_/g' | sed 's/[,.]/_/g')
 		if [ "$old_md" != "$new_md" ]; then
-			mv "$old_md" "$new_md"
+			git mv "$old_md" "$new_md"
 		fi
 	done
 }
