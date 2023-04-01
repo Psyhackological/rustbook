f5423a711953c222aad487e20f2b8ac56913b4b5
Psyhackological <konradkon@duck.com>
2023-04-01 18:56:14 +0200

    Fibonacci matrix exponentiation method

--- code/exercises/ch3/fibonacci/src/lib.rs
+++ code/exercises/ch3/fibonacci/src/lib.rs
@@ -1,25 +1,75 @@
-pub struct Fibonacci {
-    prev_prev: u64,
-    prev: u64,
+use num_bigint::BigUint;
+use num_traits::{One, Zero};
+
+// The matrix exponentiation method relies on the following relation:
+// | F(n+1) |   | 1 1 |^n   | F(1) |
+// | F(n)   | = | 1 0 |   * | F(0) |
+pub struct Fibonacci;
+
+impl Default for Fibonacci {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl Fibonacci {
     pub fn new() -> Self {
-        Fibonacci {
-            prev_prev: 0,
-            prev: 1,
-        }
+        Fibonacci
     }
-}
 
-impl Iterator for Fibonacci {
-    type Item = u64;
+    // Compute the nth Fibonacci number using the matrix exponentiation method.
+    pub fn nth(&self, n: usize) -> BigUint {
+        if n == 0 {
+            return BigUint::zero();
+        }
 
-    fn next(&mut self) -> Option<Self::Item> {
-        let current = self.prev_prev + self.prev;
-        self.prev_prev = self.prev;
-        self.prev = current;
+        // Initialize the Fibonacci matrix.
+        let mut matrix = vec![
+            vec![BigUint::one(), BigUint::one()],
+            vec![BigUint::one(), BigUint::zero()],
+        ];
 
-        Some(self.prev_prev)
+        // Compute the matrix raised to the power (n - 1).
+        self.matrix_pow(&mut matrix, n - 1);
+        matrix[0][0].clone()
+    }
+
+    // Multiply two matrices of BigUint elements.
+    fn matrix_mult(&self, a: &[Vec<BigUint>], b: &[Vec<BigUint>]) -> Vec<Vec<BigUint>> {
+        let a00 = &a[0][0] * &b[0][0] + &a[0][1] * &b[1][0];
+        let a01 = &a[0][0] * &b[0][1] + &a[0][1] * &b[1][1];
+        let a10 = &a[1][0] * &b[0][0] + &a[1][1] * &b[1][0];
+        let a11 = &a[1][0] * &b[0][1] + &a[1][1] * &b[1][1];
+
+        // Return the result as a new matrix.
+        vec![vec![a00, a01], vec![a10, a11]]
+    }
+
+    // Compute the matrix raised to the power n using the binary exponentiation method.
+    fn matrix_pow(&self, matrix: &mut Vec<Vec<BigUint>>, n: usize) {
+        if n == 0 || n == 1 {
+            return;
+        }
+
+        // Make a copy of the matrix.
+        let mut temp_matrix = matrix.clone();
+
+        // Compute the matrix raised to the power (n / 2) recursively.
+        self.matrix_pow(&mut temp_matrix, n / 2);
+
+        // Square the matrix.
+        let temp_matrix_squared = self.matrix_mult(&temp_matrix, &temp_matrix);
+
+        // If n is even, the result is the squared matrix.
+        if n % 2 == 0 {
+            *matrix = temp_matrix_squared;
+        } else {
+            // If n is odd, the result is the product of the squared matrix and the original matrix.
+            let fib_matrix = vec![
+                vec![BigUint::one(), BigUint::one()],
+                vec![BigUint::one(), BigUint::zero()],
+            ];
+            *matrix = self.matrix_mult(&temp_matrix_squared, &fib_matrix);
+        }
     }
 }
--- code/exercises/ch3/fibonacci/src/main.rs
+++ code/exercises/ch3/fibonacci/src/main.rs
@@ -1,25 +1,16 @@
 use fibonacci::Fibonacci;
-use std::io::{self, Write};
 
 fn main() {
-    print!("Enter the position (n) of the Fibonacci number: ");
-    io::stdout().flush().unwrap(); // Flush stdout to make sure the prompt is shown before read_line
-
+    println!("Enter the position (n) of the Fibonacci number: ");
     let mut input = String::new();
-    io::stdin()
+    std::io::stdin()
         .read_line(&mut input)
         .expect("Failed to read input");
-
     let n: usize = input
         .trim()
         .parse()
         .expect("Failed to parse the input as an integer");
 
-    let fib_n = Fibonacci::new().nth(n).unwrap();
-    match n {
-        1 => println!("The {}-st Fibonacci number is: {}", n, fib_n),
-        2 => println!("The {}-nd Fibonacci number is: {}", n, fib_n),
-        3 => println!("The {}-rd Fibonacci number is: {}", n, fib_n),
-        _ => println!("The {}-th Fibonacci number is: {}", n, fib_n),
-    }
+    let fib_n = Fibonacci::new().nth(n);
+    println!("The {}-th Fibonacci number is: {}", n, fib_n);
 }
