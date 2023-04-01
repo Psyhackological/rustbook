5d4a47a094c963951ab2122ea95ef62b05b2534a
Psyhackological <konradkon@duck.com>
2023-04-01 20:08:47 +0200

    Moved some part of the main.rs to lib.rs

--- code/exercises/ch3/temp_converter/src/lib.rs
+++ code/exercises/ch3/temp_converter/src/lib.rs
@@ -1,1 +1,19 @@
+pub enum Temperature {
+    Celsius(f64),
+    Fahrenheit(f64),
+}
 
+impl Temperature {
+    pub fn convert(&self) -> Self {
+        match self {
+            Temperature::Celsius(c) => {
+                let f = c * 9.0 / 5.0 + 32.0;
+                Temperature::Fahrenheit(f)
+            }
+            Temperature::Fahrenheit(f) => {
+                let c = (f - 32.0) * 5.0 / 9.0;
+                Temperature::Celsius(c)
+            }
+        }
+    }
+}
--- code/exercises/ch3/temp_converter/src/main.rs
+++ code/exercises/ch3/temp_converter/src/main.rs
@@ -1,24 +1,5 @@
 use std::io::{self, Write};
-
-enum Temperature {
-    Celsius(f64),
-    Fahrenheit(f64),
-}
-
-impl Temperature {
-    fn convert(&self) -> Self {
-        match self {
-            Temperature::Celsius(c) => {
-                let f = c * 9.0 / 5.0 + 32.0;
-                Temperature::Fahrenheit(f)
-            }
-            Temperature::Fahrenheit(f) => {
-                let c = (f - 32.0) * 5.0 / 9.0;
-                Temperature::Celsius(c)
-            }
-        }
-    }
-}
+use temp_converter::Temperature;
 
 fn main() {
     print!("Enter the temperature value followed by its unit (C or F): ");
