5fd3dc98419e59f11828c6958fb0c65e8364309b
Psyhackological <konradkon@duck.com>
2023-04-02 21:37:48 +0200

    Added some comments to department

--- code/exercises/ch8/department/src/lib.rs
+++ code/exercises/ch8/department/src/lib.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
 
+// Define a Company struct that will store department data
 pub struct Company {
     data: HashMap<String, Vec<String>>,
 }
 
+// Implement the Default trait for Company, allowing us to create an instance with default values
 impl Default for Company {
     fn default() -> Self {
         Self::new()
@@ -11,22 +13,31 @@ impl Default for Company {
 }
 
 impl Company {
+    // Constructor for the Company struct
     pub fn new() -> Company {
         Company {
+            // Initialize an empty HashMap to store departments and their employees
             data: HashMap::new(),
         }
     }
 
+    // Add an employee to a department
     pub fn add_employee(&mut self, employee: &str, department: &str) {
         self.data
             .entry(department.to_string())
+            // If the department exists, use its employee list; otherwise, create a new Vec
             .or_insert_with(Vec::new)
+            // Add the employee to the department's list
             .push(employee.to_string());
     }
 
+    // Remove an employee from a department
     pub fn remove_employee(&mut self, employee: &str, department: &str) -> Result<(), String> {
+        // Check if the department exists in the HashMap
         if let Some(employees) = self.data.get_mut(department) {
+            // Find the index of the employee in the department's list
             if let Some(index) = employees.iter().position(|e| e == employee) {
+                // Remove the employee from the list
                 employees.remove(index);
                 return Ok(());
             } else {
@@ -39,6 +50,7 @@ impl Company {
         Err(format!("Department {} not found.", department))
     }
 
+    // List all employees in a department, sorted alphabetically
     pub fn list_department(&self, department: &str) -> Option<Vec<String>> {
         self.data.get(department).map(|employees| {
             let mut sorted = employees.clone();
@@ -47,6 +59,7 @@ impl Company {
         })
     }
 
+    // List all employees in the company by department, sorted alphabetically
     pub fn list_all(&self) -> Vec<(String, Vec<String>)> {
         let mut all_departments: Vec<(String, Vec<String>)> = self
             .data
@@ -61,22 +74,28 @@ impl Company {
         all_departments
     }
 
+    // Process user input and generate output based on the command
     pub fn process_input(&mut self, input: &str) -> String {
         let parts: Vec<&str> = input.split_whitespace().collect();
 
+        // Add employee to a department
         if parts.len() >= 4 && parts[0] == "Add" && parts[2] == "to" {
             let employee = parts[1];
             let department = parts[3];
             self.add_employee(employee, department);
             format!("Added {} to {} department.", employee, department)
-        } else if parts.len() >= 4 && parts[0] == "Remove" && parts[2] == "from" {
+        }
+        // Remove employee from a department
+        else if parts.len() >= 4 && parts[0] == "Remove" && parts[2] == "from" {
             let employee = parts[1];
             let department = parts[3];
             match self.remove_employee(employee, department) {
                 Ok(()) => format!("Removed {} from the {} department.", employee, department),
                 Err(message) => message,
             }
-        } else if parts.len() == 2 && parts[0] == "List" {
+        }
+        // List employees in a specific department
+        else if parts.len() == 2 && parts[0] == "List" {
             let department = parts[1];
             match self.list_department(department) {
                 Some(employees) => {
@@ -88,7 +107,9 @@ impl Company {
                 }
                 None => format!("No employees found in the {} department.", department),
             }
-        } else if input == "List all" {
+        }
+        // List all employees in the company by department
+        else if input == "List all" {
             let all_departments = self.list_all();
             all_departments
                 .iter()
@@ -102,6 +123,7 @@ impl Company {
                 .collect::<Vec<String>>()
                 .join("\n\n")
         } else {
+            // Handle invalid commands
             String::from("Invalid command. Please try again.")
         }
     }
